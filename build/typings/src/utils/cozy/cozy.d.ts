dee83924c0949661532914a9e57a284358715c9f
Sivasankar <sivasankar.d@sap.com>
2018-09-25 21:51:35 +0530

    Add more tests

--- build/typings/src/utils/cozy/cozy.d.ts
+++ build/typings/src/utils/cozy/cozy.d.ts
@@ -1,5 +1,9 @@
 import * as dialog from "./components/Dialog";
 declare const cozy: {
+    list: (props: import("utils/cozy/components/List").MyProps) => {
+        hello: string;
+        text: string;
+    };
     dialog: typeof dialog;
 };
 export { cozy };
--- build/typings/src/utils/geometry-types.d.ts
+++ build/typings/src/utils/geometry-types.d.ts
@@ -1,5 +1,9 @@
 declare const geometry: {
     cozy: {
+        list: (props: import("utils/cozy/components/List").MyProps) => {
+            hello: string;
+            text: string;
+        };
         dialog: typeof import("utils/cozy/components/Dialog");
     };
 };
